e16d5d00b36ec2ae6576664bc4d6da6709fee2fb
LeeksooO <kyoungsoo0301@gmail.com>
2025-05-08 00:32:15 +0900

    feat(service): ChatRoomService 구현중

--- src/main/java/com/example/demo/service/ChatRoomService.java
+++ src/main/java/com/example/demo/service/ChatRoomService.java
@@ -1,5 +1,8 @@
 package com.example.demo.service;
 import org.springframework.stereotype.Service;
+
+import com.example.demo.domain.ChatRoom;
+
 import java.util.List;
 
 import java.util.concurrent.atomic.AtomicLong;
@@ -8,12 +11,20 @@ import java.util.concurrent.atomic.AtomicLong;
 public class ChatRoomService {
 
     private final AtomicLong roomIdCounter = new AtomicLong(1);
+    //private final ChatRoom chatRoom;
 
     public Long createRoom(Long matchingId, List<Long> matchedUsers) {
 
-        System.out.println("✅ CreateChattingRoom - matchingId: " + matchingId +
-                           ", users: " + matchedUsers);
+        // UUID 기반 ChatRoom 생성
+        // ChatRoom Entity에 필요한 정보 저장.
+        // ChatRoom ID 리턴
+
         
+
+
+
+
+
         // return roomId for test
         Long roomId = roomIdCounter.getAndIncrement();
 
