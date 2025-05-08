fc26c6a82e22f606b202b11c07cd112eaae422b2
LeeksooO <kyoungsoo0301@gmail.com>
2025-05-08 14:10:51 +0900

    feat(Repo): ChatRoomRepo 구현
    외부에서 UUID 기반으로 채팅방을 조회할 수 있고,
    채팅방 리스트에서 특정 유저가 속한 방을 모두 조회할 수 있습니다.
    추후 유저 UI에 유저가 속한 채팅방 리스트를 보여주는 기능을 고려하여 설계

--- src/main/java/com/example/demo/repository/ChatRoomRepository.java
+++ src/main/java/com/example/demo/repository/ChatRoomRepository.java
@@ -4,14 +4,14 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.domain.ChatRoom;
+import com.example.demo.domain.User;
 
 
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
-    Optional<ChatRoom> findByToken(String token);
-    void deleteByUsername(String username);
     
-    // 주어진 username 으로 저장된 모든 리프레시 토큰을 조회
-    List<ChatRoom> findAllByUsername(String username);
-    // 주어진 username 으로 저장된 모든 리프레시 토큰을 한 번에 삭제
-    void deleteAllByUsername(String username);
+    // 외부에서 UUID 기반 조회
+    Optional<ChatRoom> findByRoomId(String roomId);
+
+    // 특정 유저가 속한 방 모두 조회
+    List<ChatRoom> findAllByParticipantsContaining(User user); 
 }
