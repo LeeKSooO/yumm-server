6c8260375e38483e1379d63201b1913aa9a90d2a
LeeksooO <kyoungsoo0301@gmail.com>
2025-05-08 13:59:27 +0900

    feat(Entity): ChatRoom Entity 설계
    ChatRoom Entity는 매칭된 유저들이 들어갈 채팅방을 나타내는 JPA 엔티티입니다
    roomId 필드는 UUID를 이용한 외부 식별자용 필드입니다.
    URL이나 WebSocket에서 사용 시 roomId로 조회합니다.

--- src/main/java/com/example/demo/domain/ChatRoom.java
+++ src/main/java/com/example/demo/domain/ChatRoom.java
@@ -4,24 +4,34 @@ import java.util.List;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 
 @AllArgsConstructor
 @NoArgsConstructor
+@Setter
+@Getter
 @Entity
 public class ChatRoom {
     @Id @GeneratedValue
-    private Long id; // 고유 식별자(PK)
+    private Long id;
     
     private String roomId; // room id(UUID) < 외부 식별자 용
     private int maxUserCount;
 
     @ManyToMany
-    private List<User> participants;
+    private List<User> participants; // UserIds
+
+    @OneToOne
+    @JoinColumn(name = "matching_id") // 매칭id로 어떤 매칭 결과로 만들어졌는지 추척할때 사용
+    private Matching matching;
 
     @Builder
     public ChatRoom(String roomId, int maxUserCount, List<User> participants) {
@@ -29,4 +39,5 @@ public class ChatRoom {
         this.maxUserCount = maxUserCount;
         this.participants = participants;
     }
+
 }
